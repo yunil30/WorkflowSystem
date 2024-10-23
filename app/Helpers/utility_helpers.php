7ceaf5974d0113fbf2552ada316008842c7b7619
ucdungo <yunildungo@gmail.com>
2024-10-23 21:50:18 +0800

    First commit

---
+++ app/Helpers/utility_helpers.php
@@ -0,0 +1,21 @@
+<?php
+    define('BASE_URL', 'http://localhost:8020/');
+
+    if (!function_exists('host_url')) {
+        function host_url() {
+            return BASE_URL;
+        }
+    }
+
+    if (!function_exists('ShowHeader')) {
+        function ShowHeader() {
+            return view('Components/HeaderContainer');
+        }
+    }
+
+    if (!function_exists('ShowFooter')) {
+        function ShowFooter() {
+            return view('Components/FooterContainer');
+        }
+    }
+?>
---
+++ app/Views/Components/FooterContainer.php
@@ -0,0 +1,6 @@
+<!-- This is the footer section -->
+<footer>
+    <div class="copyrights">
+        <p>&copy; <?= date('Y') ?> Codeigniter Project</p>
+    </div>
+</footer>
